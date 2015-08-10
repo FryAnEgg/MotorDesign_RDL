8d6accd35c16bd562d3ee787cb3dc8b54290d03a
Dave Lathrop <dave@conceptyard.com>
2015-08-10 12:45:56 -0600

    Initial Commit

---
+++ MotorDesign_RDL/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MotorDesign_RDL
+//
+//  Created by David Lathrop on 8/10/15.
+//  Copyright (c) 2015 Fry An Egg Technology. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ MotorDesign_RDLTests/MotorDesign_RDLTests.swift
@@ -0,0 +1,36 @@
+//
+//  MotorDesign_RDLTests.swift
+//  MotorDesign_RDLTests
+//
+//  Created by David Lathrop on 8/10/15.
+//  Copyright (c) 2015 Fry An Egg Technology. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class MotorDesign_RDLTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
